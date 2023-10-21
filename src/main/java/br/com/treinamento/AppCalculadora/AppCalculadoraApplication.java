7c413b4fe5b3529cebfb152102aed9b86a309fc1
itaji-create <itaji.1999@gmail.com>
2023-10-21 14:03:56 -0300

    projeto calculadora

---
+++ src/main/java/br/com/treinamento/AppCalculadora/AppCalculadoraApplication.java
@@ -0,0 +1,20 @@
+package br.com.treinamento.AppCalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class AppCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCalculadoraApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplete() {
+		return new RestTemplate();
+	}
+
+}
---
+++ src/main/java/br/com/treinamento/AppCalculadora/resource/WebController.java
@@ -0,0 +1,49 @@
+package br.com.treinamento.AppCalculadora.resource;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+	
+	@GetMapping("/")
+	public String showForm(Model model) {
+		model.addAttribute("modelOperacoes", List.of("Soma", "Subtracao"));
+		return "form";
+	};
+	
+	@PostMapping("/")
+	public String handleFormSubmission(@RequestParam String modelOperacoes,
+			@RequestParam String valor01, 
+			@RequestParam String valor02,
+			Model model) {
+		
+		if(modelOperacoes.isEmpty() || valor01.isEmpty() || valor02.isEmpty()) {
+			return null;
+		}
+		
+		String resposta = "";
+		int resp = 0;
+		
+		switch (modelOperacoes) {
+			case "Soma":
+				resp = Integer.parseInt(valor01) + Integer.parseInt(valor02);
+				break;
+			case "Subtracao":
+				resp = Integer.parseInt(valor01) - Integer.parseInt(valor02);
+				break;
+		}
+		resposta = String.valueOf(resp);
+		
+		model.addAttribute("modelOperacoes", List.of("Soma", "Subtracao"));
+		model.addAttribute("response", resposta);
+		model.addAttribute("selectedModel", modelOperacoes);
+		return "form";
+		
+	};
+}
